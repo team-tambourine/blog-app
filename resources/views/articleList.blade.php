a1615f2529e2caac58b3a6a7537049b636251e2b
sugihara10969 <sugihara.10969@gmail.com>
2020-09-23 19:15:05 +0900

    [update]コメントの削除、foreachで回しやすくした

--- resources/views/articleList.blade.php
+++ resources/views/articleList.blade.php
@@ -3,8 +3,6 @@
 <head>
    <meta charset='UTF-8'>
    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-   <!-- <link rel='stylesheet' href="{{ asset('sass/articleList.css') }}"> -->
-   <!-- <link rel='stylesheet' href='../sass/articleList.css'> -->
    <link rel='stylesheet' href='/css/articleList.css'>
    <title>List</title>
 </head>
@@ -13,16 +11,20 @@
       <h2 class="pageTitle">記事一覧</h2>
       <div class="border"></div>
 
+<!-- 以下、foreachで回す記事雛形 -->
       <div class='listItemContainer'>
-         <!-- <img class="itemImage" /> -->
-         <div class='itemImage'></div>
-         <div class='informationContainer'>
-            <h4 class='listItemTitle'>title</h4>
-            <p class='userName'>userName</p>
-         </div>
+         <main class="flexBox">
+            <img class="itemImage" />
+            <div class='informationContainer'>
+               <h4 class='listItemTitle'>title</h4>
+               <p class='userName'>userName</p>
+            </div>
+         </main>
+         <div class="listItemBorder"></div>
       </div>
-      <div class="listItemBorder"></div>
+
+
+
    </div>
-   
 </body>
 </html>

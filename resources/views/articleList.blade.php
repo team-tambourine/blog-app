6dbc0971bd2473002cd612feb93192831a28df43
sugihara10969 <sugihara.10969@gmail.com>
2020-09-23 18:49:47 +0900

    [add]記事一覧のビュー（HTML/CSS）の雛形を作成
    /public/cssディレクトリにcssを書きました。

---
+++ resources/views/articleList.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang='ja'>
+<head>
+   <meta charset='UTF-8'>
+   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+   <!-- <link rel='stylesheet' href="{{ asset('sass/articleList.css') }}"> -->
+   <!-- <link rel='stylesheet' href='../sass/articleList.css'> -->
+   <link rel='stylesheet' href='/css/articleList.css'>
+   <title>List</title>
+</head>
+<body>
+   <div class='container'>
+      <h2 class="pageTitle">記事一覧</h2>
+      <div class="border"></div>
+
+      <div class='listItemContainer'>
+         <!-- <img class="itemImage" /> -->
+         <div class='itemImage'></div>
+         <div class='informationContainer'>
+            <h4 class='listItemTitle'>title</h4>
+            <p class='userName'>userName</p>
+         </div>
+      </div>
+      <div class="listItemBorder"></div>
+   </div>
+   
+</body>
+</html>

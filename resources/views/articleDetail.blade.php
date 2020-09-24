584c069a8ca292d81bb37c328ff81a8279f64756
sugihara10969 <sugihara.10969@gmail.com>
2020-09-24 12:37:12 +0900

    [add]articleDetail（記事詳細画面）の雛形を作成

---
+++ resources/views/articleDetail.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>articleDetail</title>
+        <link rel="stylesheet" href="/css/articleDetail.css" />
+    </head>
+    <body>
+        <div class="container">
+            <div class="userNameContainer">
+                <img class="listItemImage" src="" />
+                <div class="userName">userName</div>
+                <div class="updatedTime">2020年10月22日に更新</div>
+            </div>
+            <main>
+                <h2 class="listItemTitle">title</h2>
+                <div class="textBody">
+                    text text text text text text text text text text text text
+                    text text text text text text text text text text text text
+                    text text text text text text text text text text text text
+                    text text text text text text text text text text text text
+                    text
+                </div>
+            </main>
+            <div class="border"></div>
+            <div class="userNameContainer">
+                <img class="listItemImage" src="" />
+                <p class="userName">userName</p>
+                <p class="updatedTime">2020年10月22日に更新</p>
+            </div>
+        </div>
+    </body>
+</html>

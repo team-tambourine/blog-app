1c134975dd425777597e9f2c2455338c7ccda185
hukupon <hukupon0528@gmail.com>
2020-09-24 14:22:19 +0900

    ログイン・ユーザー登録機能

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,8 @@ Route::get('/', function () {
     return view('articleList');
     // return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+

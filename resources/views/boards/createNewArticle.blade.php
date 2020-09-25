5e8ec9b7d596d40a0f558e27eff2cc5904e461e8
sugihara10969 <sugihara.10969@gmail.com>
2020-09-25 14:40:03 +0900

    [add]createNewArticleにreset.cssを入れた

--- resources/views/boards/createNewArticle.blade.php
+++ resources/views/boards/createNewArticle.blade.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>CreateNewArticle</title>
+        <link rel="stylesheet" href="/css/reset.css">
         <link
             rel="stylesheet"
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"

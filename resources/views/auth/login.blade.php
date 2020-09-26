3767f8adbe98e5be14c908a3100f31d8d236740d
mokupon <58970874+mokupon@users.noreply.github.com>
2020-09-27 08:50:40 +0900

    ログイン画面とユーザ登録画面をデフォルト状態からフロントさんのデザインに変更 (#20)
    * ログイン画面とユーザ登録画面をデフォルト状態からフロントさんのデザインに変更
    ログイン画面からユーザ登録画面に遷移、その逆も出来るようにしました。リンクの部分でHTML,CSSが少し変わっているので、ご確認お願い致します。
    
    * ログイン・ユーザ登録にて、formのactionの追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,56 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/reset.css">
+        <link rel="stylesheet" href="/css/loginAndSignUp.css">
+    </head>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <body>
+        @include('header')
+        <div class="container">
+          <div class="loginContent">
+            <div class="text-center">
+                  <h1 class="loginTitle">ログイン</h1>
             </div>
+          
+            <div class="row formContainer">
+              <!-- <div class="col-sm-6 offset-sm-3"> -->
+              <div class="formWrapper">
+                  <form method="POST" action="{{ route('login') }}" >
+                    @csrf
+                    <div class="form-group">
+                    　<p class="loginLabel">メールアドレス</p>
+                      <input class="form-control"  autocomplete="email" type="email" name="email" value="{{ old('email') }}" required>
+                    </div>
+
+                    <div class="form-group">
+                      <p class="loginLabel">パスワード</p>
+                      <input class="form-control" autocomplete="off" type="password" name="password" required>
+                      <p class="passwordDescription description">8文字以上 小文字大文字数字が入っているようにしてください。</p>
+                    </div>
+                    
+
+                    <div class="actions">
+                      <input type="submit" name="commit" value="ログイン" class="btn btn-primary w-100 submitButton">
+                    </div>
+                    
+                  </form>
+                    {{-- ユーザ登録ページへのリンク --}}
+                  <p class="mt-2 description">もしアカウントを持っていない場合は<a href="{{ route('register') }}" class="loginLink">新規登録</a>をしてください。</p>
+                
+              </div>
+            </div>   
+            
+            
         </div>
-    </div>
-</div>
-@endsection
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+    </body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,62 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>signUp</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/reset.css">
+        <link rel="stylesheet" href="/css/loginAndSignUp.css">
+    </head>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <body>
+        @include('header')
+        <div class="container">
+          <div class="loginContent">
+            <div class="text-center">
+                  <h1 class="loginTitle">tritonへようこそ</h1>
+                  <p class="loginSubTitle"> 新規登録（無料）して、利用を開始しよう。</p>
             </div>
+          
+            <div class="row formContainer">
+              <!-- <div class="col-sm-6 offset-sm-3"> -->
+              <div class="formWrapper">
+                  <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <div class="form-group">
+                      <p class="loginLabel">ユーザー名</p>
+                      <input class="form-control"  type="text" name="name" value="{{ old('name') }}" required autofocus>
+                    </div>
+                    
+                    <div class="form-group">
+                    　<p class="loginLabel">メールアドレス</p>
+                      <input class="form-control"  autocomplete="email" type="email" name="email" value="{{ old('email') }}" required>
+                    </div>
+
+                    <div class="form-group">
+                      <p class="loginLabel">パスワード</p>
+                      <input class="form-control" autocomplete="off" type="password" name="password" required>
+                      <p class="passwordDescription description">8文字以上 小文字大文字数字が入っているようにしてください。</p>
+                    </div>
+                    
+
+                    <div class="actions">
+                      <input type="submit" name="commit" value="登録する" class="btn btn-primary w-100 submitButton">
+                    </div>
+                    
+                  </form>
+                    {{-- ユーザ登録ページへのリンク --}}
+                  <p class="mt-2 description">もしアカウントを持っている場合は<a href="{{ route('login') }}" class="loginLink">ログイン</a>をしてください。</p>
+                
+              </div>
+            </div>   
+            
+            
         </div>
-    </div>
-</div>
-@endsection
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+    </body>
+</html>
